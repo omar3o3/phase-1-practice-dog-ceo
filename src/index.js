17f7f17d6d92bf3530872c0518d014b670f0357d
Omar <otantawy53@gmail.com>
2022-05-04 17:22:36 -0400

    commit tree 6f133a72919550ba5ef18b970943200b9a489156

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded' , imageLoader);
+
+const imageLoader = () =>{
+    fetch('https://dog.ceo/api/breeds/image/random/4').then(resp => resp.json()).then(data => imageAdder(data))
+}
+
+const imageAdder = (data) =>{
+    let dogContainer = document.querySelector('#dog-image-container');
+
+    data.forEach(element of data){
+        let dogPicture = document.createElement('img');
+        dogPicture.src = data.message;
+        dogContainer.append(dogPicture);
+    }
+}
